57bb1cab8f0b8d80c5fde8dd478430a1b53c4dac
Tamara Will <tamara@tamarawill.com>
2014-09-17 13:54:03 -0400

    Added first draft of index.php with associated image file.

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html>
+<head>
+	<title>Tamara's Project 1</title>
+</head>
+<body>
+<h1>Tamara Will's DWA15 Portfolio Page</h1>
+<h2>A Photo of Tamara</h2>
+<p>Here is a dorky picture of me sitting at my desk at work:</p>
+<p><img src="img/tamara.jpg" alt="Photo of Tamara Will sitting at her desk." /></p>
+<h2>Tamara's DWA15 Projects</h2>
+<h3>Project 1</h3>
+<ul>
+	<li><a href="http://p1.tamarawill.com">Live URL (This page!)</a></li>
+	<li><a href="https://github.com/tamarawill/p1">Github Repository</a></li>
+</ul>
+<h3>Project 2</h3>
+<ul>
+	<li><a href="http://p2.tamarawill.com">Live URL</a></li>
+	<li><a href="https://github.com/tamarawill/p2">Github Repository</a></li>
+</ul>
+<h3>Project 3</h3>
+<ul>
+	<li><a href="http://p3.tamarawill.com">Live URL</a></li>
+	<li><a href="https://github.com/tamarawill/p3">Github Repository</a></li>
+</ul>
+<h3>Project 4</h3>
+<ul>
+	<li><a href="http://p4.tamarawill.com">Live URL</a></li>
+	<li><a href="https://github.com/tamarawill/p4">Github Repository</a></li>
+</ul>
+
+<h2>Getting to Know Me</h2>
+
+<dt>In 2+ sentences describe who you are and what you do.</dt>
+<dd>I live in the Boston area. I'm a Web Application Support Associate at <a href="http://hms.harvard.edu" target="_blank">Harvard Medical School</a>'s IT department, and I'm working on growing my career in the direction of instructional technology and UI/UX. In my spare time, I like to do yoga - I especially enjoy the alignment-based yoga classes at <a href="http://www.backbayyoga.com" target="_blank">Back Bay Yoga</a>.</dd>
+
+<dt>Why are you taking this course?</dt>
+<dd>I'm planning to apply to the <a href="http://www.extension.harvard.edu/degrees-programs/information-technology/concentrations-courses/digital-media" target="_blank">Master's in IT concentrating in Digital Media Arts and Instructional Design</a>, and this course will count as one of the requirements. I'm excited to be learning more about version control and PHP!</dd>
+
+<dt>What previous experience do you have with programming?</dt>
+<dd>I've taken a number of Extension School courses, including courses in Java and C, and a number of scripting languages including JavaScript, JQuery, and Python. I've done a little bit of work in Wordpress and learned some basics of PHP.</dd>
+
+<dt>Are you working on a Mac, PC, or some other platform?</dt>
+<dd>I'm mostly working on a Mac running the Mountain Lion OS (OS 10.8).</dd>
+
+<dt>Anything else we should know?</dt>
+<dd>I'm originally from the Philadelphia area, but I've lived in Massachusetts for 16 years now!</dd>
+
+</body>
+</html>
