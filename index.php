a47cc8e8fa58de392c6c55ecc71fe56e0e75795f
Tamara Will <tamara@tamarawill.com>
2014-09-18 16:51:33 -0400

    Added Bootstrap styling to index.php; added style.css

--- index.php
+++ index.php
@@ -2,50 +2,107 @@
 <html>
 <head>
 	<title>Tamara's Project 1</title>
+	
+	<!-- Latest compiled and minified Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	
+	<!-- Bootstrap theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+	<!-- Local CSS -->
+	
+	<link rel="stylesheet" href="style.css">
+
 </head>
 <body>
-<h1>Tamara Will's DWA15 Portfolio Page</h1>
-<h2>A Photo of Tamara</h2>
-<p>Here is a dorky picture of me sitting at my desk at work:</p>
-<p><img src="img/tamara.jpg" alt="Photo of Tamara Will sitting at her desk." /></p>
-<h2>Tamara's DWA15 Projects</h2>
-<h3>Project 1</h3>
-<ul>
-	<li><a href="http://p1.tamarawill.com">Live URL (This page!)</a></li>
-	<li><a href="https://github.com/tamarawill/p1">Github Repository</a></li>
-</ul>
-<h3>Project 2</h3>
-<ul>
-	<li><a href="http://p2.tamarawill.com">Live URL</a></li>
-	<li><a href="https://github.com/tamarawill/p2">Github Repository</a></li>
-</ul>
-<h3>Project 3</h3>
-<ul>
-	<li><a href="http://p3.tamarawill.com">Live URL</a></li>
-	<li><a href="https://github.com/tamarawill/p3">Github Repository</a></li>
-</ul>
-<h3>Project 4</h3>
-<ul>
-	<li><a href="http://p4.tamarawill.com">Live URL</a></li>
-	<li><a href="https://github.com/tamarawill/p4">Github Repository</a></li>
-</ul>
+<div class="container-fluid">
+	<header class="row">
+	<div class="col-md-12">
+		<h1>Tamara Will's DWA15 Portfolio Page</h1>
+		</div> <!-- end col -->
+	</header>
+	<div class="row">
+	<div class="col-md-12">
 
-<h2>Getting to Know Me</h2>
+		<h2>A Photo of Tamara</h2>
+		<p>Here is a dorky picture of me sitting at my desk at work:</p>
+		<p><img src="img/tamara.jpg" alt="Photo of Tamara Will sitting at her desk." /></p>
+		<h2>Tamara's DWA15 Projects</h2>
+	</div> <!-- end col -->
+	</div> <!-- end row -->
+	<div class="row">
+	<div class="col-md-3">
+	<div class="panel panel-default">
+  		<div class="panel-heading">
+    		<h3 class="panel-title">Project 1</h3>
+  		</div>
+  		<div class="panel-body projpanel">
+				<p><a href="http://p1.tamarawill.com">Live URL</a></p>
+				<p><a href="https://github.com/tamarawill/p1">Github Repository</a></p>
+		</div> <!-- end panel body -->
+	</div> <!-- end panel-->
+	</div> <!-- end col -->
+	<div class="col-md-3">
+	<div class="panel panel-default">
+  		<div class="panel-heading">
+    		<h3 class="panel-title">Project 2</h3>
+  		</div>
+  		<div class="panel-body projpanel">
+				<p><a href="http://p2.tamarawill.com">Live URL</a></p>
+				<p><a href="https://github.com/tamarawill/p2">Github Repository</a></p>
+		</div> <!-- end panel body -->
+	</div> <!-- end panel-->
+	</div> <!-- end col -->
+	<div class="col-md-3">
+	<div class="panel panel-default">
+  		<div class="panel-heading">
+    		<h3 class="panel-title">Project 3</h3>
+  		</div>
+  		<div class="panel-body projpanel">
+				<p><a href="http://p3.tamarawill.com">Live URL</a></p>
+				<p><a href="https://github.com/tamarawill/p3">Github Repository</a></p>
+		</div> <!-- end panel body -->
+	</div> <!-- end panel-->
+	</div> <!-- end col -->
+	<div class="col-md-3">
+	<div class="panel panel-default">
+  		<div class="panel-heading">
+    		<h3 class="panel-title">Project 4</h3>
+  		</div>
+  		<div class="panel-body projpanel">
+				<p><a href="http://p4.tamarawill.com">Live URL</a></p>
+				<p><a href="https://github.com/tamarawill/p4">Github Repository</a></p>
+		</div> <!-- end panel body -->
+	</div> <!-- end panel-->
+	</div> <!-- end col -->
+	</div> <!-- end row -->
+	<div class="row">
+	<div class="col-md-12">
+	
+		<h2>Getting to Know Me</h2>
 
-<dt>In 2+ sentences describe who you are and what you do.</dt>
-<dd>I live in the Boston area. I'm a Web Application Support Associate at <a href="http://hms.harvard.edu" target="_blank">Harvard Medical School</a>'s IT department, and I'm working on growing my career in the direction of instructional technology and UI/UX. In my spare time, I like to do yoga - I especially enjoy the alignment-based yoga classes at <a href="http://www.backbayyoga.com" target="_blank">Back Bay Yoga</a>.</dd>
+		<dt>In 2+ sentences describe who you are and what you do.</dt>
+		<dd>I live in the Boston area. I'm a Web Application Support Associate at <a href="http://hms.harvard.edu" target="_blank">Harvard Medical School</a>'s IT department, and I'm working on growing my career in the direction of instructional technology and UI/UX. In my spare time, I like to do yoga - I especially enjoy the alignment-based yoga classes at <a href="http://www.backbayyoga.com" target="_blank">Back Bay Yoga</a>.</dd>
 
-<dt>Why are you taking this course?</dt>
-<dd>I'm planning to apply to the <a href="http://www.extension.harvard.edu/degrees-programs/information-technology/concentrations-courses/digital-media" target="_blank">Master's in IT concentrating in Digital Media Arts and Instructional Design</a>, and this course will count as one of the requirements. I'm excited to be learning more about version control and PHP!</dd>
+		<dt>Why are you taking this course?</dt>
+		<dd>I'm planning to apply to the <a href="http://www.extension.harvard.edu/degrees-programs/information-technology/concentrations-courses/digital-media" target="_blank">Master's in IT concentrating in Digital Media Arts and Instructional Design</a>, and this course will count as one of the requirements. I'm excited to be learning more about version control and PHP!</dd>
 
-<dt>What previous experience do you have with programming?</dt>
-<dd>I've taken a number of Extension School courses, including courses in Java and C, and a number of scripting languages including JavaScript, JQuery, and Python. I've done a little bit of work in Wordpress and learned some basics of PHP.</dd>
+		<dt>What previous experience do you have with programming?</dt>
+		<dd>I've taken a number of Extension School courses, including courses in Java and C, and a number of scripting languages including JavaScript, JQuery, and Python. I've done a little bit of work in Wordpress and learned some basics of PHP.</dd>
 
-<dt>Are you working on a Mac, PC, or some other platform?</dt>
-<dd>I'm mostly working on a Mac running the Mountain Lion OS (OS 10.8).</dd>
+		<dt>Are you working on a Mac, PC, or some other platform?</dt>
+		<dd>I'm mostly working on a Mac running the Mountain Lion OS (OS 10.8).</dd>
 
-<dt>Anything else we should know?</dt>
-<dd>I'm originally from the Philadelphia area, but I've lived in Massachusetts for 16 years now!</dd>
+		<dt>Anything else we should know?</dt>
+		<dd>I'm originally from the Philadelphia area, but I've lived in Massachusetts for 16 years now!</dd>
+		</div> <!-- end col -->
+	</div> <!-- end div class row -->
+</div> <!-- end div class container-fluid -->
 
+<!-- JQuery Javascript -->
+<script src="https://code.jquery.com/ui/1.11.1/jquery-ui.min.js"></script>
+
+<!-- Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </body>
 </html>
